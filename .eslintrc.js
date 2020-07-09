4258385a3a7b76af1d249d32117b6fa284d4dc33
LightMotive <andrelight.al@gmail.com>
2020-07-08 18:35:21 -0600

    Install and configure eslint

---
+++ .eslintrc.js
@@ -0,0 +1,20 @@
+/* eslint-env node */
+
+module.exports = {
+  root: true,
+  parser: "@typescript-eslint/parser",
+  plugins: ["@typescript-eslint"],
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "prettier/@typescript-eslint",
+  ],
+  overrides: [
+    {
+      files: ["*.js"],
+      rules: {
+        "@typescript-eslint/no-var-requires": "off",
+      },
+    },
+  ],
+};
