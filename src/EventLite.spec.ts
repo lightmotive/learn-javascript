cf7b2b3d0cc5a4b86167444b0b291359d2167502
LightMotive <andrelight.al@gmail.com>
2020-07-08 18:51:54 -0600

    Configure unit testing
    - Mocha, chai, ts-node

---
+++ src/EventLite.spec.ts
@@ -0,0 +1,11 @@
+import { expect } from "chai";
+import "mocha";
+
+//Temporary demo test
+describe("This", () => {
+  describe("should", () => {
+    it("always pass", () => {
+      expect(true).to.equal(false);
+    });
+  });
+});
